9d05e75eff412ef8d6af40febe34736992d5cc17
mgolshenas <105927485+mgolshenas@users.noreply.github.com>
2025-05-25 15:56:40 +0100

    Update and rename 1. Fourier_Spectrum.py to 1. Module_Fourier_Spectrum.py

---
+++ 1. Module_Fourier_Spectrum.py
@@ -0,0 +1,9 @@
+# fourier_spectrum.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+ 
+def calculate_2dft(input):
+    ft = np.fft.ifftshift(input)
+    ft = np.fft.fft2(ft)
+    return np.fft.fftshift(ft)
